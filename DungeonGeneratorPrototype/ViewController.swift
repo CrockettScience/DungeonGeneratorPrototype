63189844d6beebc16b26958fc11952d10e9276bd
Guest User <Guest@3400-34.instruction.cuesta.org>
2017-04-25 12:30:40 -0700

    Initial Commit

---
+++ DungeonGeneratorPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DungeonGeneratorPrototype
+//
+//  Created by Guest User on 4/25/17.
+//  Copyright © 2017 Cuesta College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
